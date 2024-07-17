e824bcaa0591883e3b79fedf160e50c39d6c7f7e
PinkGoosik <MCGycuk@yandex.ru>
2024-07-17 14:46:01 +0500

    update to 1.21

---
+++ src/main/java/tallflowerpots/mixin/DecoratedPotMixin.java
@@ -0,0 +1,30 @@
+package tallflowerpots.mixin;
+
+import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DecoratedPotBlock.class)
+abstract class DecoratedPotMixin {
+
+	@Inject(method = "onUseWithItem", at = @At("HEAD"), cancellable = true)
+	void onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ItemActionResult> cir) {
+		if(hit.getSide().equals(Direction.UP) && stack.getItem() instanceof BlockItem blockItem) {
+			if(blockItem.getBlock() instanceof TallPlantBlock || blockItem.getBlock() instanceof TwistingVinesBlock || blockItem.getBlock().equals(Blocks.PITCHER_PLANT) || blockItem.getBlock() instanceof SweetBerryBushBlock) {
+				cir.setReturnValue(ItemActionResult.SKIP_DEFAULT_BLOCK_INTERACTION);
+			}
+		}
+	}
+
+}
